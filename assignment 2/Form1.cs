2cee0c94a2b8f9f5cb884a05253b937c167c2d67
jacob thornton <Jet2002Jake@gmail.com>
2018-10-16 09:50:40 +0100

    added 5 resistors and put them in a dropbox

--- assignment 2/Form1.cs
+++ assignment 2/Form1.cs
@@ -16,5 +16,69 @@ namespace assignment_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double R1 = Convert.ToDouble(textBox1.Text);
+            double R2 = Convert.ToDouble(textBox2.Text);
+            double R3 = Convert.ToDouble(textBox3.Text);
+            double R4 = Convert.ToDouble(textBox4.Text);
+            double R5 = Convert.ToDouble(textBox5.Text);
+            double Rt = R1 + R2 + R3 + R4 + R5;
+            label1.Text = "the answer is " + Rt;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+
+                textBox1.Show();
+                textBox2.Show();
+                label1.Show();
+                button1.Show();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                textBox4.Hide();
+                textBox5.Hide();
+
+                textBox3.Show();
+                textBox1.Show();
+                textBox2.Show();
+                label1.Show();
+                button1.Show();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                textBox5.Hide();
+
+                textBox4.Show();
+                textBox3.Show();
+                textBox1.Show();
+                textBox2.Show();
+                label1.Show();
+                button1.Show();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                textBox5.Show();
+                textBox4.Show();
+                textBox3.Show();
+                textBox1.Show();
+                textBox2.Show();
+                label1.Show();
+                button1.Show();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
